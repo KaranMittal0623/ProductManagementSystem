4de71db59834d45aac37e416a5c94dac641852c5
Karan Mittal <Karanmittal662@gmail.com>
2025-04-08 22:52:24 +0530

    Demo class uploaded

---
+++ DairyProduct/src/AddMilkmenKaran.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.sql.*;
+
+public class AddMilkmenKaran {
+
+//    All variables declaration
+    public static int Id=0;
+    private int MilkmenId;
+    private String Name;
+    private String PhoneNumber;
+    private String Area;
+    private ArrayList<String> DeliveryArea;
+    private double rating;
+    private ArrayList<String> MilkTypes;
+    private int experience;
+
+
+
+//    Adding a new MilkMen
+    AddMilkmenKaran(){
+
+    }
+    AddMilkmenKaran(String Name, String PhoneNumber, String Area){
+        this.MilkmenId = Id++;
+        this.Name = Name;
+        this.PhoneNumber = PhoneNumber;
+        this.Area = Area;
+        this.DeliveryArea = new ArrayList<>();
+        this.rating = 0.0;
+        this.MilkTypes = new ArrayList<>();
+        this.experience = 0;
+    }
+
+
+//    Function to take input from the user of details
+    public AddMilkmenKaran userInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("-------Adding new Milkmen--------");
+        System.out.print("Enter the Milkmen name: ");
+        String Name = input.nextLine();
+        System.out.print("Enter the PhoneNumber: ");
+        String PhoneNumber = input.nextLine();
+        System.out.print("Enter the Area: ");
+        String Area = input.nextLine();
+
+        AddMilkmenKaran newMilkmen = new AddMilkmenKaran(Name,PhoneNumber,Area);
+        addToDatabase();
+        return newMilkmen;
+
+    }
+
+
+//    Getters for all Variables
+    public String getName(){
+        System.out.print(this.Name);
+        return this.Name;
+    }
+    public String getPhoneNumber(){
+        return this.PhoneNumber;
+    }
+    public String getArea(){
+        return this.Area;
+    }
+
+//    Setters for some variable
+    public void addDeliveryArea(String Area){
+        if(!this.DeliveryArea.contains(Area)){
+            DeliveryArea.add(Area);
+        }
+    }
+
+
+
+//    Add data to database
+    public void addToDatabase() {
+        try {
+            ConnKaran conn = new ConnKaran();
+            String query = "insert into Milkmen value('"+Name+"', '"+PhoneNumber+"','"+Area+"')";
+            conn.s.executeUpdate(query);
+            System.out.println("Data added successfully");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
--- DairyProduct/src/ConnKaran.java
+++ DairyProduct/src/ConnKaran.java
@@ -1,2 +1,19 @@
+import java.net.ConnectException;
+import java.sql.*;
+
 public class ConnKaran {
+    Connection c;
+    Statement s;
+    ConnKaran(){
+
+//        In try block it is a connection of java with the database MySql
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/productmanagementsystem","root","Karan@123786");
+            s = c.createStatement();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
